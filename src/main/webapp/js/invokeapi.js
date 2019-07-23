fd2f8d40929772c9e4ecf80fbf8cb9ae14ee96fb
Sujith Sreekumar <Sujith.Sreekumar@signicat.com>
2019-07-23 16:47:13 +0200

    added Dr.Deepadevi

--- src/main/webapp/js/invokeapi.js
+++ src/main/webapp/js/invokeapi.js
@@ -1,8 +1,14 @@
 var LeaveLine = function () {
     var self = this;
+    // self.doctors = $.getJSON("http://localhost:8080/smsbooking/webapi/resources/doctors", function (data) {
+    //     return ko.mapping.fromJS(data);
+    // });
+    // console.log(self.doctors);
+    self.valueString1 = ko.observable();
+    self.value2 = ko.observable();
     self.department = ko.observable();
     self.doctor = ko.observable();
-    self.department.subscribe(function() {
+    self.department.subscribe(function () {
         self.doctor(undefined);
     });
 };
@@ -10,12 +16,14 @@ var LeaveLine = function () {
 var LeaveModel = function () {
     var self = this;
     self.lines = ko.observableArray([new LeaveLine()]);
-
-    //operations
-    self.addLine = function() { self.lines.push(new LeaveLine())};
-    self.removeLine = function(line) { self.lines.remove(line) };
-    self.save = function() {
-        var dataToSave = $.map(self.lines(), function(line) {
+    self.addLine = function () {
+        self.lines.push(new LeaveLine())
+    };
+    self.removeLine = function (line) {
+        self.lines.remove(line)
+    };
+    self.save = function () {
+        var dataToSave = $.map(self.lines(), function (line) {
             return line.doctor() ? {
                 doctorName: line.doctor().name,
                 departmentName: line.department.department
@@ -23,8 +31,13 @@ var LeaveModel = function () {
         });
         alert("Could now send this to server: " + JSON.stringify(dataToSave));
     };
+
+    ViewModel.prototype.to01012000 = function() {
+        this.valueString1('01/01/2000');
+        this.value2(new Date(2000, 0, 1));
+    };
 };
 
-$(function() {
-    ko.applyBindings(new LeaveModel());
-});
+
+ko.applyBindings(new LeaveModel());
+
